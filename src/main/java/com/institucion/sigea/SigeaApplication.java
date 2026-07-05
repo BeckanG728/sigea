14d6199042091f13224a86a5c78a2603ae006f28
BeckanG728 <209495631+BeckanG728@users.noreply.github.com>
2026-07-05 11:51:20 -0500

    fix: problema en version de dependencia y atributo de entidad usuario

--- src/main/java/com/institucion/sigea/SigeaApplication.java
+++ src/main/java/com/institucion/sigea/SigeaApplication.java
@@ -2,8 +2,10 @@ package com.institucion.sigea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SigeaApplication {
 
     public static void main(String[] args) {
